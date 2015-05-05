d73d5e18a629d3cadebd0e266ee139cf70b18e9a
irons163 <irons@ironsdeMacBook-Air.local>
2015-05-05 18:22:28 +0800

    Initial Commit

---
+++ try_5connectTests/try_5connectTests.m
@@ -0,0 +1,34 @@
+//
+//  try_5connectTests.m
+//  try_5connectTests
+//
+//  Created by irons on 2015/5/5.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface try_5connectTests : XCTestCase
+
+@end
+
+@implementation try_5connectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
